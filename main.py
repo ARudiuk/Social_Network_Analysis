702d209fad76e871ed7c1e0b4ac5b8681c39cf1b
HsoulT <alxanderr@gmail.com>
2012-11-26 22:52:29 -0400

    connectivity,path, centrality

--- main.py
+++ main.py
@@ -31,7 +31,45 @@ def list_component_sizes(G):
     marked = {}
     for node in G.keys():
         if node not in marked:
-            print "Component containing", node, ": ", mark_component(G, node, marked)
+            print "Component containing", node, ": ", mark_component(G, node, marked
+#check connectivity
+def check_connection(G, v1, v2):
+    marked = {}
+    mark_component(G, v1, marked)
+    return v2 in marked
+#find path between two nodes, breadth first
+def path(G, v1, v2):
+    #distance_from_start = {}
+    path_from_start = {} # modification
+    open_list = [v1]
+    #distance_from_start[v1] = 0
+    path_from_start[v1] = [v1] # modification
+    while len(open_list) > 0:
+        current = open_list[0]
+        del open_list[0]
+        for neighbor in G[current].keys():
+            #if neighbor not in distance_from_start:
+            if neighbor not in path_from_start: # modification
+                #distance_from_start[neighbor] = distance_from_start[current] + 1
+                path_from_start[neighbor] = path_from_start[current] 
+                                              + [neighbor] # modification
+                #if neighbor == v2: return distance_from_start[v2]
+                if neighbor == v2: return path_from_start[v2] # modification
+                open_list.append(neighbor)
+    return False
+#centrality
+def centrality(G, v):
+    distance_from_start = {}
+    open_list = [v]
+    distance_from_start[v] = 0
+    while len(open_list) > 0:
+        current = open_list[0]
+        del open_list[0]
+        for neighbor in G[current].keys():
+            if neighbor not in distance_from_start:
+                distance_from_start[neighbor] = distance_from_start[current] + 1
+                open_list.append(neighbor)
+    return float(sum(distance_from_start.values()))/len(distance_from_start)
 # Make an empty graph
 a_ring ={}
 G={}
